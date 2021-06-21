146b5a1e6f12f75efed5de41c23662cdc630626e
thanh <lanhthanh.dktd@gmail.com>
2021-06-21 15:51:39 +0700

    install jwt

---
+++ 04. source/frontend/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup | undefined;
+  loading = false;
+  submitted = false;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
