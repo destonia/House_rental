97a8f9a370af14e0af26a0de602c56bbcdbcb920
thanh <lanhthanh.dktd@gmail.com>
2021-06-21 16:44:00 +0700

    use template

--- 04. source/frontend/src/app/app.module.ts
+++ 04. source/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HouseUpdateComponent } from './house/house-update/house-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseUpdateComponent
   ],
   imports: [
     BrowserModule,
